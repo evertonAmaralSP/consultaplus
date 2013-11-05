470344bf67d667160099cd45a198dc2f2831bc55
Everton Amaral <everton.amaral@abril.com.br>
2013-11-05 20:03:31 -0200

    Inclusao de layout e tela de login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
-  before_filter :require_logged_user,
-    :only => [:index]
+  before_filter :require_logged_user, :only => [:index]
+
+  
 	def index
 		
 	end
